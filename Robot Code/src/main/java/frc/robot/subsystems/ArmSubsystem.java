27447f007ed3749c9fa05ec850df9312ac6a389d
topsailRobotics <162211611+topsailRobotics@users.noreply.github.com>
2024-03-04 19:19:29 -0500

    Update ArmSubsystem.java

--- Robot Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ Robot Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,44 +9,148 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
 public class ArmSubsystem extends SubsystemBase {
-  // Creates a PIDController with gains kP, kI, and kD
-  private PIDController turnController = new PIDController(kP, kI, kD);
-  
   //set up shooter motors
   private final CANSparkMax m_innerarm = new CANSparkMax(ArmConstants.kInnerArmCanId, MotorType.kBrushless);
   private final CANSparkMax m_outerarm = new CANSparkMax(ArmConstants.kOuterArmCanId, MotorType.kBrushless);
+  
+  // Creates a PIDController with gains kP, kI, and kD
+  private PIDController turnController = new PIDController(kP, kI, kD);
 
-  public CANEncoder getEncoder(
+  private RelativeEncoder m_encoder;
+
+  private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
+
+  // initialze PID controller and encoder objects
+  m_pidController = m_innerarm.getPIDController();
+  m_encoder = m_innerarm.getEncoder();
   /** Creates a new IntakeSubsystem. */
   public ArmSubsystem() {
-    // Sets the error tolerance to 5, and the error derivative tolerance to 10 per second
-    turnController.setTolerance(5, 10);
+    m_motor.restoreFactoryDefaults();
 
-    // Returns true if the error is less than 5 units, and the
-    // error derivative is less than 10 units
-    turnController.atSetpoint();
-    
-    // The integral gain term will never add or subtract more than 0.5 from
-    // the total loop output
-    turnController.setIntegratorRange(-0.5, 0.5);
+ 
 
-    // Enables continuous input on a range from -180 to 180
-    turnController.enableContinuousInput(-180, 180);
+    // PID coefficients
+    kP = 5e-5; 
+    kI = 1e-6;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.000156; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
 
-    // Clamps the controller output to between -0.5 and 0.5
-    MathUtil.clamp(turnController.calculate(encoder.getDistance(), setpoint), -0.5, 0.5);
+    // Smart Motion Coefficients
+    maxVel = 2000; // rpm
+    maxAcc = 1500;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    /**
+     * Smart Motion coefficients are set on a SparkPIDController object
+     * 
+     * - setSmartMotionMaxVelocity() will limit the velocity in RPM of
+     * the pid controller in Smart Motion mode
+     * - setSmartMotionMinOutputVelocity() will put a lower bound in
+     * RPM of the pid controller in Smart Motion mode
+     * - setSmartMotionMaxAccel() will limit the acceleration in RPM^2
+     * of the pid controller in Smart Motion mode
+     * - setSmartMotionAllowedClosedLoopError() will set the max allowed
+     * error for the pid controller in Smart Motion mode
+     */
+    int smartMotionSlot = 0;
+    m_pidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
+    m_pidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
+    m_pidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
+    m_pidController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+
+    // display Smart Motion coefficients
+    SmartDashboard.putNumber("Max Velocity", maxVel);
+    SmartDashboard.putNumber("Min Velocity", minVel);
+    SmartDashboard.putNumber("Max Acceleration", maxAcc);
+    SmartDashboard.putNumber("Allowed Closed Loop Error", allowedErr);
+    SmartDashboard.putNumber("Set Position", 0);
+    SmartDashboard.putNumber("Set Velocity", 0);
+
+    // button to toggle between velocity and smart motion modes
+    SmartDashboard.putBoolean("Mode", true);
+  }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  //public void rotateDegrees(double angle) {
+  //m_innerarm.reset();
+  //m_outerarm.reset();
+  //turnController.reset();
+  //turnController.setPID(kP,kI,kD);
+  //m_innerarm.set(turnController.calculate(encoder.getDistance(), setpoint));
+  //m_outerarm.set(turnController.calculate(encoder.getDistance(), setpoint));
+  //}
   public void rotateDegrees(double angle) {
-  m_innerarm.reset();
-  m_outerarm.reset();
-  turnController.reset();
-  turnController.setPID(kP,kI,kD);
-  m_innerarm.set(turnController.calculate(encoder.getDistance(), setpoint));
-  m_outerarm.set(turnController.calculate(encoder.getDistance(), setpoint));
+   // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double maxV = SmartDashboard.getNumber("Max Velocity", 0);
+    double minV = SmartDashboard.getNumber("Min Velocity", 0);
+    double maxA = SmartDashboard.getNumber("Max Acceleration", 0);
+    double allE = SmartDashboard.getNumber("Allowed Closed Loop Error", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+    if((maxV != maxVel)) { m_pidController.setSmartMotionMaxVelocity(maxV,0); maxVel = maxV; }
+    if((minV != minVel)) { m_pidController.setSmartMotionMinOutputVelocity(minV,0); minVel = minV; }
+    if((maxA != maxAcc)) { m_pidController.setSmartMotionMaxAccel(maxA,0); maxAcc = maxA; }
+    if((allE != allowedErr)) { m_pidController.setSmartMotionAllowedClosedLoopError(allE,0); allowedErr = allE; }
+
+    double setPoint, processVariable;
+    boolean mode = SmartDashboard.getBoolean("Mode", false);
+    if(mode) {
+      setPoint = SmartDashboard.getNumber("Set Velocity", 0);
+      m_pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+      processVariable = m_encoder.getVelocity();
+    } else {
+      setPoint = SmartDashboard.getNumber("Set Position", 0);
+      /**
+       * As with other PID modes, Smart Motion is set by calling the
+       * setReference method on an existing pid object and setting
+       * the control type to kSmartMotion
+       */
+      m_pidController.setReference(setPoint, CANSparkMax.ControlType.kSmartMotion);
+      processVariable = m_encoder.getPosition();
+    }
+    
+    SmartDashboard.putNumber("SetPoint", setPoint);
+    SmartDashboard.putNumber("Process Variable", processVariable);
+    SmartDashboard.putNumber("Output", m_motor.getAppliedOutput());
   }
 }
