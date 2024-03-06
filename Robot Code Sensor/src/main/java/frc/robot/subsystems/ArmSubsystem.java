63ec95fde4fd77894fbdf88690961d423aacb6c1
topsailRobotics <162211611+topsailRobotics@users.noreply.github.com>
2024-03-05 20:46:21 -0500

    Add files via upload

---
+++ Robot Code Sensor/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import frc.robot.Constants.ArmConstants;
+
+public class ArmSubsystem extends SubsystemBase {
+
+  //set up shooter motors
+  private final CANSparkMax m_innerarm = new CANSparkMax(ArmConstants.kInnerArmCanId, MotorType.kBrushless);
+  private final CANSparkMax m_outerarm = new CANSparkMax(ArmConstants.kOuterArmCanId, MotorType.kBrushless);
+
+  /** Creates a new IntakeSubsystem. */
+  public ArmSubsystem() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  
+}
---
+++ Robot Code Sensor/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import frc.robot.Constants.WheelConveyorConstants;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  //set up instake motor
+  private final CANSparkMax m_intake = new CANSparkMax(WheelConveyorConstants.kIntakeCanId, MotorType.kBrushless);
+
+  /** Creates a new IntakeSubsystem. */
+  public IntakeSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void stopIntake() {
+      m_intake.setVoltage(0);
+    }
+
+  public void runIntake() {
+      m_intake.setVoltage(12);
+  }
+
+  public void runIntakeIfSensor() {
+    try (DigitalInput m_sensor = new DigitalInput(0)) {
+      if (m_sensor.get()){
+          m_intake.setVoltage(0);
+      } else {
+          m_intake.setVoltage(12);
+      }
+    }
+  }
+
+}
